26887e0226accdd0cb75c43dd7238495353c4589
Dann38 <38dunn@gmail.com>
2019-08-20 19:33:02 +0800

    Все работает: добавил ссылки, подгрузил static

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import mainapp.views as mainapp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', mainapp.main),
+    path('contacts/', mainapp.contacts),
+    path('products/', mainapp.products),
+    path('product/', mainapp.product),
+
 ]
