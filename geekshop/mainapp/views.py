2377534a46eb866253c80fc2b73ffe4be8447c74
Dann38 <38dunn@gmail.com>
2019-08-24 14:18:49 +0800

    подредактировал файл views в соответствие с добавлением меню продукта

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 menu = [
     {'href': '/', 'name': 'home'},
-    {'href': '/products', 'name': 'products'},
+    {'href': '/products/all', 'name': 'products'},
     {'href': '/contacts', 'name': 'contacts'},
 ]
+
 def main(request):
     context = {
         'title':'Главная',
@@ -23,9 +24,19 @@ def contacts(request):
 
 
 def products(request):
+    product_type = [
+        {'href': '/products/all', 'name': 'ALL'},
+        {'href': '/products/home', 'name': 'HOME'},
+        {'href': '/products/office', 'name': 'OFFICE'},
+        {'href': '/products/furniture', 'name': 'FURNITURE'},
+        {'href': '/products/modern', 'name': 'MODERN'},
+        {'href': '/products/classic', 'name': 'CLASSIC'},
+    ]
+
     context = {
         'title': 'Продукты',
         "list_menu": menu,
+        'product_list_menu': product_type
     }
     return render(request, 'mainapp/products.html', context=context)
 
