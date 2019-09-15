ed71a35e537a3bd4b544cadb5482a854c0323a06
Dann38 <38dunn@gmail.com>
2019-09-15 19:05:55 +0800

    коммит перед пушем

--- geekshop/adminapp/forms.py
+++ geekshop/adminapp/forms.py
@@ -1,9 +1,22 @@
 from django import forms
 from authapp.models import ShopUser
 from authapp.forms import ShopUserEditForm
+from mainapp.models import CategoryProducts
 
 
 class ShopUserAdminEditForm(ShopUserEditForm):
     class Meta:
         model = ShopUser
         fields = '__all__'
+
+
+class ProductCategoryEditForm(forms.ModelForm):
+    class Meta:
+        models = CategoryProducts
+        fields = '__all__'
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            for field_name, field in self.fields.items():
+                field.widget.attrs['class'] = 'form-control'
+                field.help_text = ''
